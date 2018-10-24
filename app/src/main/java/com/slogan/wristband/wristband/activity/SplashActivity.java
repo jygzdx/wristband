32e77ba9cac39e25fb5c08465530519753c7eb0e
chenzebin <chenhaihai@mishuoapp.com>
2018-10-24 18:03:10 +0800

    添加登录

--- app/src/main/java/com/slogan/wristband/wristband/activity/SplashActivity.java
+++ app/src/main/java/com/slogan/wristband/wristband/activity/SplashActivity.java
@@ -1,27 +1,43 @@
 package com.slogan.wristband.wristband.activity;
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.os.Message;
+import android.widget.ImageView;
 
 import com.slogan.wristband.wristband.R;
 import com.slogan.wristband.wristband.activity.base.BaseActivity;
-import com.slogan.wristband.wristband.utils.DisplayUtils;
 import com.slogan.wristband.wristband.utils.StatusBarCompat;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SplashActivity extends BaseActivity {
+
+    @BindView(R.id.iv_logo)
+    ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
         StatusBarCompat.compat(this);
         initHandler();
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) btn_count_down.getLayoutParams();
 //            layoutParams.topMargin = DisplayUtils.getStatusBarHeight() + DisplayUtils.dip2px(this, 24);
 //        }
+    }
+
+    @Override
+    public void handleMessageInfo(Message message) {
+        super.handleMessageInfo(message);
+
+    }
+
+    @OnClick(R.id.iv_logo)
+    public void onViewClicked() {
+
     }
 }
