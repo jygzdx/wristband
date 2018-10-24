8df89d59dbb3b710e462a8db73425dcb1f4c2234
chenzebin <644974853@qq.com>
2018-10-24 23:36:58 +0800

    fix

--- app/src/main/java/com/slogan/wristband/wristband/activity/LoginActivity.java
+++ app/src/main/java/com/slogan/wristband/wristband/activity/LoginActivity.java
@@ -1,15 +1,40 @@
 package com.slogan.wristband.wristband.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.slogan.wristband.wristband.R;
+import com.slogan.wristband.wristband.activity.base.BaseActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.tv_login)
+    TextView tvLogin;
+    @BindView(R.id.tv_register)
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_login, R.id.tv_register})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_login:
+                Intent loginIntent = new Intent(LoginActivity.this,LoginMainActivity.class);
+                startActivity(loginIntent);
+                break;
+            case R.id.tv_register:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/slogan/wristband/wristband/widght/PasswordLoginView.java
@@ -0,0 +1,22 @@
+package com.slogan.wristband.wristband.widght;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.slogan.wristband.wristband.R;
+
+/**
+ * Created by free_boy on 2018/10/24.
+ */
+
+public class PasswordLoginView extends LinearLayout {
+    private final Context mContext;
+private View view ;
+    public PasswordLoginView(Context context) {
+        super(context);
+        this.mContext = context;
+        view = LayoutInflater.from(mContext).inflate(R.layout., this);
+    }
+}
